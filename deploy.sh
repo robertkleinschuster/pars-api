913d67068aafc7c803dec1e518822bdee2f0f4bc
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-04-08 18:18:36 +0200

    updates

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export PATH=/opt/plesk/php/7.4/bin:$PATH:$HOME/bin
 if [ -d "vendor" ]; then
+    rm composer.lock
   php /usr/lib64/plesk-9.0/composer.phar update --no-dev --no-interaction &>deploy.log
 else
   php /usr/lib64/plesk-9.0/composer.phar install --no-dev --no-interaction &>deploy.log
