5810632302522004c8d4d72bd6ede664a3a427cf
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-04-08 21:06:31 +0200

    updates

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@ name='pars-api'
 
 export PATH=/opt/plesk/php/7.4/bin:$PATH:$HOME/bin
 
-
-if [ ! -f $name ]; then
+if [ ! -d "$name" ]; then
   git clone https://github.com/pars-framework/$name
   php /usr/lib64/plesk-9.0/composer.phar install --no-dev --no-interaction &>deploy-new.log
 fi
