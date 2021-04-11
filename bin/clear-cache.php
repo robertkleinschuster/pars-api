6ede193cee7dce67ad2b4a729ec2ecbff5dc3ff1
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-04-11 17:10:04 +0200

    dependency refactoring

--- bin/clear-cache.php
+++ bin/clear-cache.php
@@ -13,16 +13,16 @@ require 'vendor/autoload.php';
     $adapter = $container->get(\Laminas\Db\Adapter\AdapterInterface::class);
     $translator = $container->get(\Laminas\I18n\Translator\TranslatorInterface::class);
 
-    $cache = new \Pars\Core\Deployment\Cache($container->get('config'), $adapter);
+    $cache = $container->get(\Pars\Core\Deployment\CacheClearer::class);
     $cache->setTranslator($translator);
     $now = new DateTime();
-    $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_RESET_OPCACHE);
-    $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_CLEAR_IMAGES);
-    $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_CLEAR_ASSETS);
-    $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_CLEAR_CACHE_POOL);
-    $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_CLEAR_CONFIG);
+    $cache->removeOption(\Pars\Core\Deployment\CacheClearer::OPTION_RESET_OPCACHE);
+    $cache->removeOption(\Pars\Core\Deployment\CacheClearer::OPTION_CLEAR_IMAGES);
+    $cache->removeOption(\Pars\Core\Deployment\CacheClearer::OPTION_CLEAR_ASSETS);
+    $cache->removeOption(\Pars\Core\Deployment\CacheClearer::OPTION_CLEAR_CACHE_POOL);
+    $cache->removeOption(\Pars\Core\Deployment\CacheClearer::OPTION_CLEAR_CONFIG);
     if ($now->format('H:i') != '00:00') {
-        $cache->removeOption(\Pars\Core\Deployment\Cache::OPTION_CLEAR_BUNDLES);
+        $cache->removeOption(\Pars\Core\Deployment\CacheClearer::OPTION_CLEAR_BUNDLES);
     }
     $cache->clear();
     echo 'Cache Cleared';
